a88f4ef59429c6696377d1d0d80967ad6e2c11a4
Anthony Suárez <neolight1010@gmail.com>
2025-05-18 19:11:38 -0500

    Set up http server

--- src/neo_api/core.clj
+++ src/neo_api/core.clj
@@ -1,8 +1,28 @@
 (ns neo-api.core
-  (:require [neo-api.config :as config]))
+  (:require [neo-api.config :as config]
+            [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
+
+(defn -respond-hello [_request]
+  {:status 200 :body "Hello, world!"})
+
+(def -routes
+  (route/expand-routes
+   #{["/greet" :get -respond-hello :route-name :greet]}))
+
+(defn -create-server [config]
+  (http/create-server
+   {::http/routes -routes
+    ::http/type :jetty
+    ::http/join? false
+    ::http/port (-> config :server :port)}))
+
+(defn -start-server [config]
+  (http/start (-create-server config)))
 
 (defn -main
   []
   (let [config  (config/read-config)]
-    (println "🚀 server started")
-    (println "config:" config)))
+    (println "🚀 starting server")
+    (println "config:" config)
+    (-start-server config)))
