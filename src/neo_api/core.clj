a8e72fe896768515c0075ce6ee6158c88379bee8
Anthony Suárez <neolight1010@gmail.com>
2025-05-31 21:32:33 -0500

    Set up actual pedestal-component

--- src/neo_api/core.clj
+++ src/neo_api/core.clj
@@ -1,31 +1,14 @@
 (ns neo-api.core
   (:require
    [com.stuartsierra.component :as component]
-   [io.pedestal.http :as http]
-   [io.pedestal.http.route :as route]
    [neo-api.components.example-component :as example-component]
+   [neo-api.components.pedestal-component :as pedestal-component]
    [neo-api.config :as config]))
-
-(defn -respond-hello [_request]
-  {:status 200 :body "Hello, world!"})
-
-(def -routes
-  (route/expand-routes
-   #{["/greet" :get -respond-hello :route-name :greet]}))
-
-(defn -create-server [config]
-  (http/create-server
-   {::http/routes -routes
-    ::http/type :jetty
-    ::http/join? false
-    ::http/port (-> config :server :port)}))
-
-(defn -start-server [config]
-  (http/start (-create-server config)))
 
 (defn api-system [config]
   (component/system-map
-   :example-component (example-component/new-example-component config)))
+   :example-component (example-component/new-example-component config)
+   :pedestal-component (component/using (pedestal-component/new-pedestal-component config) [:example-component])))
 
 (defn -main
   []
