21da3b80647218be31826d95836663824cf44fb2
Anthony Suárez <neolight1010@gmail.com>
2025-06-16 12:34:47 -0500

    Re-export reset fn

--- dev/dev.clj
+++ dev/dev.clj
@@ -5,3 +5,6 @@
 (component-repl/set-init
  (fn [_old-system]
    (core/api-system {:server {:port 3001}})))
+
+#_{:clojure-lsp/ignore [:clojure-lsp/unused-public-var]}
+(def reset component-repl/reset)
